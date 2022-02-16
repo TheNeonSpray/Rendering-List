9d44b7c3903d273c4f91b49caf122a0f115547e2
TheNeonSpray <alexandraeb20@gmail.com>
2022-02-16 15:27:00 -0600

    practice of conditional rendering with an array of objects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const jsonData = [
+  { key: "1", name: "Alex" },
+  { key: "2", name: "Max" },
+  { key: "3", name: "Rose" },
+  { key: "4", name: "Valerie" },
+];
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false)
+    }, 4000)
+  },[])
+
+  const renderData = () => {
+    
+    //value is what you iterate
+    return jsonData.map((value) => {
+      // this is a callback that return a component
+      return (
+        <div>
+          <ul className="list-group">
+            <li key={value.key} className="list-group-item">
+              {value.name}
+            </li>
+          </ul>
+        </div>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5">
+      <h1 className="text-center">List of people</h1>
+      {/* Ternary (If isLoading is true show, otherwise show*/}
+      {isLoading ? <h2>The data is loading...</h2> : renderData()}
     </div>
   );
 }
